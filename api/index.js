576c2c9991bb11a19711b8ab3ad654d9ba90b2d1
iabiy <lijabiy@gmail.com>
2019-10-03 12:44:38 -0400

    added sequel cli

---
+++ api/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+const PORT = process.env.PORT || 8000;
+
+//Default welcome page
+app.get('*', (req, res) => {
+    res.status(200).send({
+        message: "welcome to API"
+    });
+})
+
+app.listen(PORT, () => {
+    console.log('Server is running on PORT ${PORT}');
+});
+export default app;
